27a71f4997c84d3899ed0b95e1f61f4a9d2bcdd4
bv101rj <robert@janey.tech>
2024-09-09 12:52:15 -0400

    Actually works, need to add an invert switch for the mpv call

--- radr.py
+++ radr.py
@@ -3,23 +3,47 @@
 import requests
 import json 
 import os
-import 
+import subprocess
 
+def get_gif(site):
+    
+    #URL that the loop gif is at1
+    url = "https://radar.weather.gov/ridge/standard/{site_code}_loop.gif".format(site_code=site)
 
-def get_gif():
+    #Get a response from the server
+    response = requests.get(url, stream=True)
 
+    #Place the GIF in tmp
+    local_file = os.path.join("/tmp", "radar.gif")
+
+    #Open and write the data
+    with open(local_file, 'wb') as file:
+        for chunk in response.iter_content(chunk_size=8192):
+            if chunk:
+                file.write(chunk)
+
+    return local_file 
+
+def loop_gif(local_file):
+    #Takes local file as input, loops over it with mpv 
+    command = "mpv /tmp/radar.gif --loop=inf --no-border --osc=no"
+
+    #Loop using mpv
+    process = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
 def config(home_dir, config_dir, config_file_path):
+    
     # Get user input
-    print(f"You can navigate to radar.weather.gov you need to find your local station, upper left hand corner symbol looks like a copy button select local, click on what is close to you, it should have a 4 letter code")
-    radar_location = input("Paste the that code here: ")
+    print(f"You can navigate to radar.weather.gov you need to find your local station, upper left hand corner symbol looks like a copy button, select local, click on what is close to you, it should have a 4 letter code")
+    radar_location = input("Paste the that 4 letter code here: ")
     
     #Create a place to save the config json
     home_dir = os.path.expanduser("~")
     config_dir = os.path.join(home_dir, ".config", "radr")
     os.makedirs(config_dir, exist_ok=True)
     config_file_path = os.path.join(config_dir, "config.json")
-
+    
+    #Save the info to a json 
     config_data = {
             "configured": True,
             "radar_loc": radar_location,
@@ -40,14 +64,22 @@ def check_config(config_file_path):
             return None 
 
 def main():
+    
+    #Map out storage location 
     home_dir = os.path.expanduser("~")
     config_dir = os.path.join(home_dir, ".config", "radr")
     config_file_path = os.path.join(config_dir, "config.json")
-    cfig = check_config
+    
+    #Check if we are configured
+    cfig = check_config(config_file_path)
 
+    # If we cfig is None, run the config, only will happen once. 
     if cfig == None :
         config(home_dir, config_dir, config_file_path)
-
     else :
-        get_gif()
+        #Get out loop gif for a given radar station. 
+        file = get_gif(cfig[1])
+        loop_gif(file)
 
+if __name__ == "__main__":
+    main()
